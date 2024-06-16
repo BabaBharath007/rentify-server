060a1671582e5e0bfd736ba42b5e868ab7fac339
Bharath <v.b.yoooogo@gmail.com>
2024-06-16 18:03:51 +0530

    email

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI , UploadFile, File
 from secrets import token_hex
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import image, user,property
+from app.routers import image, user,property,email
 from app.db.database import engine, Base
 from passlib.context import CryptContext
+import smtplib
+
 
 app =FastAPI()
 
@@ -26,5 +28,6 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 app.include_router(property.app, tags=['Property'], prefix='/api/property')
 app.include_router(user.app, tags=['Users'], prefix='/api/users')
 app.include_router(image.app,tags=['Image'],prefix='/api/image')
+app.include_router(email.app,tags=['Blogs'],prefix='/api/about')
 
 
